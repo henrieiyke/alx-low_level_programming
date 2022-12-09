30e7cd5b675968bef8baf5ec3b7a716aa1f609c1
henrieiyke <okoliehenry1@gmail.com>
2022-12-09 05:20:30 +0100

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,25 +5,10 @@
 /**
  * main - Entry point
  * Description: 'the program's description'
- * Return: Always 0(Success)
+ * Return: Always 1(Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-#include <stdio.h>
-#include <unistd.h>
-
-/**
- * main - Entry point
- *
- * Description: prints exactly and that piece of art is useful
- * Return: 1
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
-
