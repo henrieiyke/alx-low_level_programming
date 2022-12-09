eb1ad5d8ea57ab90ddd184f733dc4b4232a33cf2
henrieiyke <okoliehenry1@gmail.com>
2022-12-09 04:49:13 +0100

    name

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main - Entry point
  * Return:Always 0
@@ -6,12 +6,12 @@
 main(void)
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n",size of(char));
-	printf("Size of a int: %i byte(s)\n",size of(int));
-	printf("Size of a long int: %i byte(s)\n",size of(long int));
-	printf("Size of a long long int: %i byte(s)\n",size of(long long int));
+	printf("Size of a char: %i byte(s)\n", size of(char));
+	printf("Size of a int: %i byte(s)\n", size of(int));
+	printf("Size of a long int: %i byte(s)\n", size of(long int));
+	printf("Size of a long long int: %i byte(s)\n", size of(long long int));
 	printf("Size of a float: %i byte(s)\n",size of(float));
-	printf("Size of a long long int: %i byte(s)\n",size of(long long int));
-	printf("Size of a float: %i byte(s)\n",size of(float));
+	printf("Size of a long long int: %i byte(s)\n", size of(long long int));
+	printf("Size of a float: %i byte(s)\n", size of(float));
 	return (0);
 }
