707723a1927f505c4813fe30b0a1eba44160c671
henrieiyke <okoliehenry1@gmail.com>
2022-12-10 05:04:17 +0100

    continue

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print lowervase a-z
+ *
+ * Description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
