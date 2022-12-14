6aa3a73aa349af2a8d90faeab97fe06319ee387d
henrieiyke <okoliehenry1@gmail.com>
2022-12-14 04:55:20 +0100

    continue

---
+++ 0x02-functions_nested_loops/101-natural.s
@@ -0,0 +1,93 @@
+	.file	"101-natural.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	jmp	.L2
+.L5:
+	movl	-8(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1431655766, %rax, %rax
+	shrq	$32, %rax
+	movq	%rax, %rdx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	movl	%edx, %esi
+	subl	%eax, %esi
+	movl	%esi, %eax
+	movl	%eax, %edx
+	addl	%edx, %edx
+	addl	%eax, %edx
+	movl	%ecx, %eax
+	subl	%edx, %eax
+	testl	%eax, %eax
+	je	.L3
+	movl	-8(%rbp), %ecx
+	movslq	%ecx, %rax
+	imulq	$1717986919, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %edx
+	sarl	%edx
+	movl	%ecx, %eax
+	sarl	$31, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	movl	%eax, %edx
+	sall	$2, %edx
+	addl	%eax, %edx
+	movl	%ecx, %eax
+	subl	%edx, %eax
+	testl	%eax, %eax
+	jne	.L4
+.L3:
+	movl	-8(%rbp), %eax
+	addl	%eax, -4(%rbp)
+.L4:
+	addl	$1, -8(%rbp)
+.L2:
+	cmpl	$1023, -8(%rbp)
+	jle	.L5
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
---
+++ 0x02-functions_nested_loops/102-fibonacci.s
@@ -0,0 +1,85 @@
+	.file	"102-fibonacci.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%ld"
+.LC1:
+	.string	", %ld"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	$0, -20(%rbp)
+	movq	$1, -16(%rbp)
+	movq	$2, -8(%rbp)
+	jmp	.L2
+.L6:
+	cmpl	$0, -20(%rbp)
+	jne	.L3
+	movq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L4
+.L3:
+	cmpl	$1, -20(%rbp)
+	jne	.L5
+	movq	-8(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L4
+.L5:
+	movq	-16(%rbp), %rax
+	addq	%rax, -8(%rbp)
+	movq	-8(%rbp), %rax
+	subq	-16(%rbp), %rax
+	movq	%rax, -16(%rbp)
+	movq	-8(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L4:
+	addl	$1, -20(%rbp)
+.L2:
+	cmpl	$49, -20(%rbp)
+	jle	.L6
+	movl	$10, %edi
+	call	putchar@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
