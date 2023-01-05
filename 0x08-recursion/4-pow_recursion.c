ffd14539c360c4a5f8acac59a69059c489f6c6d2
henrieiyke <okoliehenry1@gmail.com>
2023-01-05 03:38:20 +0100

    Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,7 +5,7 @@
  * _pow_recursion - return power
  * @x: input x value
  * @y: input y value
- * 
+ *
  * Description: returns the value of x raised
  * to the power of y
  * Return: returns the value of x
